6bc7f0b8b8b2f88de57b1ff3a569b0fdaea804fe
samuel <samuel.firsth@gmail.com>
2026-08-17 16:30:18 +0400

    Refine game messages and instructions

--- craps.py
+++ craps.py
@@ -1,25 +1,24 @@
 import random
 import sys
 
-ans = input("To start the game write 'yes' to quit write 'no' \n ")
+ans = input("To start the game write 'yes' to quit write 'no'\n")
 if ans.lower() == "no":
     sys.exit()
 else:
-    print("So,let's start the game!")
+    print("So,let's get the instructions of the game.")
 
 #to continue with or without construction
 
-cont = input("Welcome! If you want the game instructions write 'yes',otherwise write 'no' \n ")
+cont = input("If you want the game instructions write 'yes',otherwise write 'no'\n ")
 
 if cont.lower() == "yes":
     print("Here are the rules of the game: " \
     "The player should roll two dice. If the sum of both of them is 7 or 11 the player wins. " \
     "If the sum is 2, 3 or 12 (craps) the casino wins. If during the first roll the sum is 4, 5, 6, 8, 9 or 10, that number becomes the “goal” number. " \
-    "To win, the player should roll the dice till they roll the goal number again. If the player rolls a 7 before rolling the goal number, they lose.")
+    "To win, the player should roll the dice till they roll the goal number again. If the player rolls a 7 before rolling the goal number, they lose.Let's start!  \n \n")
 
 elif cont.lower() == "no":
-    print("Best luck!")
-
+    print("Good. Let's start the game! \n")
    
 def roll_dice():
     die1 = random.randint(1, 6)
@@ -35,12 +34,12 @@ if total in (7, 11):
     result = "Congratulations, you won!"
 
 elif total in (2, 3, 12):
-    result = "You lost, try again!"
+    result = "Casino wins!"
 
 else:
     result = "Continue trying.."
     point = total
-    print("Your point is:", point)
+    print("Your goal is:", point)
 
     while result == "Continue trying..":
         total = roll_dice()
